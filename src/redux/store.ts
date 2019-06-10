f4a70989004610fdac035af86b8cd3f06de25a36
Diego Mijelshon <diego@mijelshon.com.ar>
2019-06-10 18:58:53 -0300

    Scuttlebot POC

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,12 @@
 import { createStore } from 'redux'
 // import { logger } from './logger'
 import { reducers } from './reducers'
+import scuttlebutt from 'redux-scuttlebutt'
 
-export const store = createStore(reducers)
-
+export const store = createStore(
+  reducers,
+  undefined,
+  scuttlebutt({
+    uri: 'http://localhost:2222',
+  })
+)
